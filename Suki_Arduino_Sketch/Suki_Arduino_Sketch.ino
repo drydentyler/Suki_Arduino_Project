bb4ea6312a627968495d6f2a93e4e0cbf633b28d
drydentyler <drydentyler@gmail.com>
2023-03-20 20:23:46 -0400

    replaced solenoid with servo and inserted servo code to update

--- Suki_Arduino_Sketch/Suki_Arduino_Sketch.ino
+++ Suki_Arduino_Sketch/Suki_Arduino_Sketch.ino
@@ -1,23 +1,17 @@
 #include "pitches.h"
- 
- 
- //const int ledPin = 13;
- const int photocellPin = 0;  // the cell and 10K pulldown are connected to a0
+#include <Servo.h>
 
- int photocellReading;     // the analog reading from the sensor divider
- int duration =250;  // 500 miliseconds
-
- const int solenoidPin = 5;
-
- int melody[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-
+const int photocellPin = 0; //sets pin that will be receiving and writing to the serial monitor the light readings
+int photocellReading;       //creates the variable the sensor will read to and print on monitor
+int duration =250;          //duration between each note of the scale to be played
+int melody[] = {NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};  //C major scale that will play twice when triggered
+Servo myservo;
+int pos = 90;               //sets initial servo position to 90
 
 void setup(void) 
 {
   Serial.begin(9600);   
-  //pinMode(ledPin, OUTPUT);
-  pinMode(solenoidPin,OUTPUT);
-  digitalWrite(solenoidPin,HIGH);
+  myservo.attach(9);
 }
  
 void loop(void) 
@@ -25,29 +19,36 @@ void loop(void)
   photocellReading = analogRead(photocellPin); 
   Serial.print("Light sensor reading: ");
   Serial.println(photocellReading);
-  
-  if(photocellReading > 100) 
+  if(photocellReading >= 18)                                  //the sensor will take a reading each second and if it is 18 or above, the below will be triggered
   {
-    delay(5000);
-      
-    for(int repetitions = 0; repetitions < 2; repetitions++)
+    delay(10000);                                             //wait 10 seconds first because of the delay with the camera
+    for(int repetitions = 0; repetitions < 2; repetitions++)  //sets the repetitions of the scale to be 2
     {
-      for (int thisNote = 0; thisNote < 8; thisNote++) 
+      for (int thisNote = 0; thisNote < 8; thisNote++)        //plays all 8 notes in the scale
       {
         tone(8, melody[thisNote], duration);
         delay(200);
       }
     } 
-    delay(1000);
-    digitalWrite(solenoidPin,LOW);
-    delay(100);
-    digitalWrite(solenoidPin,HIGH);
-    delay(100);
-    delay(5000);
+  delay(1000);
+
+for(pos = 90; pos >= 0; pos-=1){                              //servo will move in 1 degree increments down to 0 from 90, the servo movements are what dispenses the treat
+    myservo.write(pos);
+    delay(15);
+  }
+for (pos = 0; pos <= 180; pos += 1) {                        //servo will move in 1 degree increments to 180
+  myservo.write(pos);              
+  delay(15);                      
+  }
+for (pos = 180; pos >= 90; pos -= 1) {                       //servo will move in 1 degree increments back to 90
+    myservo.write(pos);              
+    delay(15);                       
+  }  
+
+delay(5000);                                                  //after triggering, it will wait an additional 5 seconds to see if the camera is still in use
   }
   else 
   {
-  //digitalWrite(ledPin, LOW);
-  delay(100);
+  delay(1000);                                                //the light sensor will check the light level every second, any shorter and there could be flashes that trigger everything
   }
 }
